65ff540af2b584f512251b70a7b0a69301fa370a
kgolston13 <kgolston13@gmail.com>
2024-04-15 09:33:13 -0400

    Update AirportManager.java
    Deleted duplicate import

--- src/csci360flightsystem/AirportManager.java
+++ src/csci360flightsystem/AirportManager.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.List;
 import java.util.Objects;
